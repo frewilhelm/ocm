3b523f5ffb61f4c58f0e50558e7a13f5d085270c
Frederic Wilhelm <frederic.wilhelm@sap.com>
2026-04-15 11:16:48 +0200

    Revert "restore oras pusher"
    This reverts commit 27ac7c2e85ac67d9ff58b211dbd82547db4bf9d0.
    
    Signed-off-by: Frederic Wilhelm <frederic.wilhelm@sap.com>

--- api/tech/oras/pusher.go
+++ api/tech/oras/pusher.go
@@ -5,10 +5,9 @@ import (
 	"fmt"
 
 	"github.com/containerd/errdefs"
+	ociv1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"oras.land/oras-go/v2/registry"
 	"oras.land/oras-go/v2/registry/remote/auth"
-
-	ociv1 "github.com/opencontainers/image-spec/specs-go/v1"
 
 	"ocm.software/ocm/api/oci/ociutils"
 )
@@ -49,18 +48,23 @@ func (c *OrasPusher) Push(ctx context.Context, d ociv1.Descriptor, src Source) (
 		// that layer resulting in the created tag pointing to the right
 		// blob data.
 		if err := repository.PushReference(ctx, d, reader, c.ref); err != nil {
-			if !errdefs.IsAlreadyExists(err) {
-				return fmt.Errorf("failed to push tag: %w", err)
-			}
+			return fmt.Errorf("failed to push tag: %w", err)
 		}
 
 		return nil
 	}
 
+	ok, err := repository.Exists(ctx, d)
+	if err != nil {
+		return fmt.Errorf("failed to check if repository %q exists: %w", ref.Repository, err)
+	}
+
+	if ok {
+		return errdefs.ErrAlreadyExists
+	}
+
 	if err := repository.Push(ctx, d, reader); err != nil {
-		if !errdefs.IsAlreadyExists(err) {
-			return fmt.Errorf("failed to push: %w, %s", err, c.ref)
-		}
+		return fmt.Errorf("failed to push: %w, %s", err, c.ref)
 	}
 
 	return nil
